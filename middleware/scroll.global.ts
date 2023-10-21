fbbb30bada78590ff1887557f6c85904a40042f8
ㅤㅤ <hi@aexhell.life>
2023-10-21 16:17:25 +0200

    feat(middleware): await route

--- middleware/scroll.global.ts
+++ middleware/scroll.global.ts
@@ -1,4 +1,7 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
+   const router = useRouter();
+   await router.isReady();
+
    if (to.path !== from.path && process.client) {
       let main = document.getElementById('__AMILLIONCITIES-CONTENT');
       setTimeout(() => main.scrollTo(0, 0), 400);
