455bdce94962aa0b4483bef4eb764d633bf8cabf
ㅤㅤ <hi@aexhell.life>
2023-12-01 17:59:43 +0100

    fix(components): grayscale

--- components/Project.vue
+++ components/Project.vue
@@ -1,8 +1,8 @@
 <template>
-   <component :is="componentToDisplay" :to="`/works/${data.code}`" :class="{ 'relative': column, 'absolute': !column, 'grayscale': !active }" class="__EIGHTEEN-PROJECT z-50 lg:py-42 py-24 w-full h-auto transition-all">
+   <component :is="componentToDisplay" :to="`/works/${data.code}`" :class="{ 'relative': column, 'absolute': !column }" class="__EIGHTEEN-PROJECT z-50 lg:py-42 py-24 w-full h-auto transition-all">
       <img :alt="`${data.title} Image`" :class="{ 'object-top': data.code === 'moon'}" class="__EIGHTEEN-PROJECT-IMAGE pointer-events-none select-none absolute transition-all top-0 left-0 object-cover w-full h-full" :src="`/projects/${data.code}.webp`" />
       <div class="__EIGHTEEN-PROJECT-TITLE opacity-0 absolute transition-all top-5 left-4 text-black">
-         <div class="project-line border-t border-x-0 border-b-0 border-solid border-black h-px w-[48px] pb-4" />
+         <div :style="`width: ${Math.floor(Math.random() * 200)+4}px`" class="project-line border-t border-x-0 border-b-0 border-solid border-black h-px pb-4" />
          <p class="mt-0 mb-px text-xs uppercase">[ {{ data.position }} ]</p>
          <h2 class="mt-0 mb-4 md:text-6xl text-lg" v-text="data.title" />
          <p class="text-xs md:text-base md:block hidden" v-text="data.description" />
